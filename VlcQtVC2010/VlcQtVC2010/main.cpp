29908d9e94d9c717f2a6ad808d2cde5dd629eaf8
linmingren <linmingren2003@163.com>
2012-03-10 17:51:59 +0800

    create VC2010 project

---
+++ VlcQtVC2010/VlcQtVC2010/main.cpp
@@ -0,0 +1,16 @@
+﻿/* libVLC and Qt sample code
+ * Copyright © 2009 Alexander Maringer <maringer@maringer-it.de>
+ */
+
+#include "vlc_on_qt.h"
+#include <QtGui/QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Player p;
+    p.resize(640,480);
+    p.playFile("d:\\11.ts"); // Replace with what you want to play
+    p.show();
+    return a.exec();
+}
---
+++ VlcQtVC2010/VlcQtVC2010/vlc_on_qt.h
@@ -0,0 +1,51 @@
+﻿/* libVLC and Qt sample code
+ * Copyright © 2009 Alexander Maringer <maringer@maringer-it.de>
+ */
+#ifndef VLC_ON_QT_H
+#define VLC_ON_QT_H
+
+#include <vlc/vlc.h>
+
+#include <QtGui/QWidget>
+
+class QVBoxLayout;
+class QPushButton;
+class QTimer;
+class QFrame;
+class QSlider;
+
+#define POSITION_RESOLUTION 10000
+
+class Player : public QWidget
+{
+    Q_OBJECT
+    QSlider *_positionSlider;
+    QSlider *_volumeSlider;
+    // [20101201 Ondra Spilka] 
+    // on Linux/Ubuntu Lucid and VLC >= 1.0 (at least 1.1.5 was tested) XWindow handle have to be passed
+    // therefore QX11EmbedContainer have to be used instead of QFrame
+#ifdef Q_WS_X11    
+    QX11EmbedContainer *_videoWidget;
+#else
+    QFrame *_videoWidget;
+#endif   
+    QTimer *poller;
+    bool _isPlaying;
+   
+    libvlc_instance_t *_vlcinstance;
+    libvlc_media_player_t *_mp;
+    libvlc_media_t *_m;
+
+public:
+    Player();
+    ~Player();
+    
+
+public slots:
+    void playFile(QString file);
+    void updateInterface();
+    void changeVolume(int newVolume);
+    void changePosition(int newPosition);
+
+};
+#endif
